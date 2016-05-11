8d52e78f6c3559a71ae8d6596ddc2211da77ddf0
labby <rp@cms-lab.com>
2016-05-11 17:07:16 +0200

    modify link

--- info.php
+++ info.php
@@ -36,7 +36,7 @@ $module_version		= '1.1.0';
 $module_platform	= '2.1';
 $module_license		= 'GNU General Public License';
 $module_author		= 'Julian Schuh, Bernd Michna, "Herr Rilke", Dietrich Roland Pehlke (last)';
-$module_home		= 'http://addon.websitebaker.org/pages/en/browse-add-ons.php?type=1';
+$module_home		= 'http://www.lepton-cms.com/lepador/modules/forum.php';
 $module_guid		= '44CF11ED-D38A-4B51-AF80-EE95F7C4C00D';
 $module_description	= 'Dieses Modul integriert ein einfaches Forum in ihre Webseite.<br/>';
 
