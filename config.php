6599653f384594e6dba478bd71b3ca7fb1ad5433
aldus <info@drp-x-work.de>
2016-05-16 18:31:50 +0200

    Update files to 1.2.0.

--- config.php
+++ config.php
@@ -29,28 +29,36 @@ if (defined('LEPTON_PATH')) {
 }
 // end include class.secure.php
 
+// insert settings if not exist
+$sql = "SELECT * from `".TABLE_PREFIX."mod_forum_settings` WHERE `section_id` = ".$section_id;
+$query_settings = $database->query($sql);
+if ($query_settings === false || $query_settings->numRows()  == 0) {
+	$sql = "INSERT INTO `".TABLE_PREFIX."mod_forum_settings` VALUES(0,".$section_id.", 5, 5, 0, 1, 1, 1, 1, 1, 30, 0, '', 'admin@admin.de', 'WEBSite Forum', 1, 0, '')";
+	$database->query($sql);
+} 
+
 // Get Settings from DB
 $settings = $query_settings->fetchRow( MYSQL_ASSOC );
 
-// Einträge, die in der Themenübersicht je Seite angezeigt werden sollen
+// Eintr√§ge, die in der Themen√ºbersicht je Seite angezeigt werden sollen
 define('FORUMDISPLAY_PERPAGE', $settings['FORUMDISPLAY_PERPAGE']);
 
-// Einträge, die je Seite in einem Thema angezeigt werden sollen
+// Eintr√§ge, die je Seite in einem Thema angezeigt werden sollen
 define('SHOWTHREAD_PERPAGE', $settings['SHOWTHREAD_PERPAGE']);
 
-// Legt fest, ob für die Zahlen in der Seitennavigation verschiedene Schriftgröﬂen verwendet werden sollen
+// Legt fest, ob f√ºr die Zahlen in der Seitennavigation verschiedene Schriftgr√∂Ô¨Çen verwendet werden sollen
 define('PAGENAV_SIZES', $settings['PAGENAV_SIZES']);
 
 // Unterforen auf der Foren-Startseite anzeigen?
 define('DISPLAY_SUBFORUMS', $settings['DISPLAY_SUBFORUMS']);
 
-// Unterforen in der Themenübersicht anzeigen?
+// Unterforen in der Themen√ºbersicht anzeigen?
 define('DISPLAY_SUBFORUMS_FORUMDISPLAY', $settings['DISPLAY_SUBFORUMS_FORUMDISPLAY']);
 
-// Sollen für Gäste Captchas zum schreiben verwendet werden?
+// Sollen f√ºr G√§ste Captchas zum schreiben verwendet werden?
 define('FORUM_USE_CAPTCHA', $settings['FORUM_USE_CAPTCHA']);
 
-// ID der Gruppe der Administratoren (Dürfen Beiträge + Themen ändern/löschen)
+// ID der Gruppe der Administratoren (D√ºrfen Beitr√§ge + Themen √§ndern/l√∂schen)
 define('ADMIN_GROUP_ID', $settings['ADMIN_GROUP_ID']);
 
 // Soll das Suchformular angezeigt werden ?
@@ -62,7 +70,7 @@ define('FORUM_MAX_SEARCH_HITS', $settings['FORUM_MAX_SEARCH_HITS']);
 // sollen Mails versendet werden, wenn neue Posts eingehen?
 define('FORUM_SENDMAILS_ON_NEW_POSTS', $settings['FORUM_SENDMAILS_ON_NEW_POSTS']);
 
-// Diese Adresse bei neuen Beiträgen informieren?'
+// Diese Adresse bei neuen Beitr√§gen informieren?'
 define('FORUM_ADMIN_INFO_ON_NEW_POSTS', $settings['FORUM_ADMIN_INFO_ON_NEW_POSTS']);
 
 // Sender of notification emails on new posts
---
+++ templates/index.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ *
+ *	@module			Forum
+ *	@version		0.5.9
+ *	@authors		Julian Schuh, Bernd Michna, "Herr Rilke", Dietrich Roland Pehlke (last)
+ *	@license		GNU General Public License
+ *	@platform		2.8.x
+ *	@requirements	PHP 5.4.x and higher
+ *
+ */
+
+header("Location: ../../../index.php");
+
+?>
